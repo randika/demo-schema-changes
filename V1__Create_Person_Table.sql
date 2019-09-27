3c1fe91715b04d629ebcfdf9b8373d7541977af5
Randika Rathugamage <rrathugama@wiley.com>
2019-09-27 11:23:51 +0530

    Updated SQL

---
+++ V1__Create_Person_Table.sql
@@ -0,0 +1,5 @@
+/* Single line comment */
+CREATE TABLE Person (
+  name VARCHAR(25) NOT NULL,
+  PRIMARY KEY(name)
+);
---
+++ V2__Insert_Person_Table.sql
@@ -0,0 +1,3 @@
+insert into Person (name) values ('John');
+insert into Person (name) values ('Foo');
+insert into Person (name) values ('Bar');
