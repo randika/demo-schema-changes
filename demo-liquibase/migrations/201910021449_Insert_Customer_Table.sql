33f729ba5eec1d379ccae74d715f0d05bb5998c5
Randika Rathugamage <randika@randika.com>
2019-10-09 17:58:24 +0530

    Update 201910021449_Insert_Customer_Table.sql

--- demo-liquibase/migrations/201910021449_Insert_Customer_Table.sql
+++ demo-liquibase/migrations/201910021449_Insert_Customer_Table.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset randika:1
+--changeset randika:1, logicalFilePath:201910021449_Insert_Customer_Table.sql
 insert into Customer(name)values('John');
 insert into Customer(name)values('Foo');
 insert into Customer(name)values('Bar');
