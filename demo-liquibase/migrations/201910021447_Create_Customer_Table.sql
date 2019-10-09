f56a634bc60e88e4f1cd36e19dc0af68d245b934
Randika Rathugamage <randika@randika.com>
2019-10-09 17:56:52 +0530

    Update 201910021447_Create_Customer_Table.sql

--- demo-liquibase/migrations/201910021447_Create_Customer_Table.sql
+++ demo-liquibase/migrations/201910021447_Create_Customer_Table.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset randika:1, logicalFilePath:migrations
+--changeset randika:1, logicalFilePath:201910021447_Create_Customer_Table.sql
 CREATE TABLE Customer (
   name VARCHAR(25) NOT NULL,
   PRIMARY KEY(name)
